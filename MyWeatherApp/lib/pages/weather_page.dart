3a34306764c6e78947f87e217a89e97219406892
ChandulaJ <114409648+ChandulaJ@users.noreply.github.com>
2023-11-11 09:57:44 +0530

    app done

--- MyWeatherApp/lib/pages/weather_page.dart
+++ MyWeatherApp/lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:myweatherapp/services/weather_services.dart';
 
 import '../models/weather_model.dart';
@@ -15,8 +16,9 @@ class _WeatherPageState extends State<WeatherPage> {
 
   final _weatherService = WeatherService('dee2d070f3f11eaf3abf4a200586cefc');
   Weather? _weather;
+
   //fetch weather data
-  _fetchWeather() async{
+  _fetchWeather() async {
     String cityName = await _weatherService.getCurrentCity();
 
     try {
@@ -24,15 +26,39 @@ class _WeatherPageState extends State<WeatherPage> {
       setState(() {
         _weather = weather;
       });
-    }catch(e){
+    } catch (e) {
       print(e);
-
-
     }
   }
 
   //weather animations
+  String getWeatherAnimations(String? mainCondition) {
+    if (mainCondition == null){ return 'assets/sunny.json';}
+    else {
+      switch (mainCondition.toLowerCase()) {
+        case "clouds":
+        case "mist":
+        case "smoke":
+        case "haze":
+        case "dust":
+        case "fog":
+          return 'assets/cloudy.json';
 
+        case "rain":
+        case "drizzle":
+        case "shower rain":
+          return "assets/sunRain.json";
+
+        case "thunderstorm":
+          return "assets/thunderstorm.json";
+        case "clear":
+          return "assets/sunny.json";
+        case "default":
+          return "assets/sunny.json";
+      }
+    }
+    return "assets/sunny.json";
+  }
 
   //init state
 
@@ -44,16 +70,18 @@ class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(_weather?.cityName ?? "loading city ..."),
+            Text(_weather?.cityName ?? "loading city ...",style: ,),
+            //animation
+            Lottie.asset(getWeatherAnimations(_weather?.mainCondition)),
+
             Text('${_weather?.temperature.round()}°C'),
 
+            Text(_weather?.mainCondition ?? "")
           ],
         ),
       ),
